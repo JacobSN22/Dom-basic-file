277910bee206465905c68b30dff94eaad65d7364
Jacob <jacobsimoninielsen@gmail.com>
2022-08-18 10:32:43 +0200

    gallerycard aflevering (Updated Javascript)

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,27 @@
 const headerOne = document.createElement("h1");
-headerOne.innerText = "hellow world";
-
 const myApp = document.getElementById("app");
+const figure = document.createElement("figure")
+const img = document.createElement("img");
+const para = document.createElement("p")
+
+headerOne.innerText = "hellow world";
 myApp.appendChild(headerOne);
 
-const figure = document.createElement("figure")
+//Laver et figure tag og giver den et ID med setAttribute
 figure.setAttribute("id","container")
-document.getElementById("app").appendChild(figure)
+myApp.appendChild(figure)
 
+//Laver et h2 tag hvis at skriver innerHTML i vores container(figure)
 const container = document.getElementById("container")
 container.innerHTML = "<h2>En meget sød hund:)</h2>"
-document.getElementById("app").appendChild(container)
+myApp.appendChild(container)
 
-const img = document.createElement("img");
+//Laver et img tag
 img.src = "https://images.photowall.com/products/57211/dog-harmony.jpg?h=380&q=75";
-document.getElementById("container").appendChild(img);
+container.appendChild(img);
 
-const para = document.createElement("p")
+//Laver et p tag
 para.innerText = "En lille sød hund til salg for 100Kr fordi den er træls"
-document.getElementById("container").appendChild(para)
+container.appendChild(para)
 
 console.log(myApp);
